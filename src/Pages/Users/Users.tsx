8f9f07051cc0d5cdbb04daf8234ae5ed9a5b13b4
charity.tarus <charity.tarus@computercastlesltd.co.ke>
2023-02-06 13:45:28 +0300

    Add users page

---
+++ src/Pages/Users/Users.tsx
@@ -0,0 +1,10 @@
+
+const Users = () => {
+  return (
+    <div className='container bg-white p-5' data-testid='users'>
+      <h4 className='my-4'> All Users</h4>
+    </div>
+  );
+};
+
+export default Users;
