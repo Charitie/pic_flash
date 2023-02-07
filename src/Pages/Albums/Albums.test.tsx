2bc9b440c6bbe3f29e893410f6590350e15f54fa
charity.tarus <charity.tarus@computercastlesltd.co.ke>
2023-02-07 12:36:41 +0300

    Add album tests

--- src/Pages/Albums/Albums.test.tsx
+++ src/Pages/Albums/Albums.test.tsx
@@ -1,35 +1,39 @@
 import store from '../../store';
-import { mockNetWorkResponse } from '../../utils/test.data';
+import { fetchAlbumById, fetchAlbumPhotos, fetchAlbums } from './albumsSlice';
 
-describe('Users page', () => {
-
-  beforeAll(() => {
-    mockNetWorkResponse();
-  });
-
+describe('albums page', () => {
+  const albumId = 1;
   it('should have an initial albums state', () => {
-    const state = store.getState().albums
+    const state = store.getState().albums;
     expect(state).toEqual({
       albums: [],
       album: {},
       albumPhotos: [],
       status: 'idle',
-    })
-  })
+    });
+  });
 
-  // it('should be able to fetch the album list', async () => {
-  //   const result = await store.dispatch(fetchAlbums());
+  it('should be able to fetch album list', async () => {
+    const result = await store.dispatch(fetchAlbums(''));
 
-  //   const users = result.payload;
-  //   console.log('ALBUMS:::', users, result.type)
+    expect(result.type).toBe('albums/fetchAlbums/fulfilled');
+    const { albums } = store.getState().albums;
+    expect(albums.length).toBeGreaterThan(0);
+  });
 
-  //   expect(result.type).toBe('albums/fetchAlbums/fulfilled');
-  //   expect(users.length).toBeGreaterThan(0);
+  it('should be able to fetch single album', async () => {
+    const result = await store.dispatch(fetchAlbumById(albumId));
 
-  //   // const state = store.getState().users;
+    expect(result.type).toBe('albums/fetchAlbumById/fulfilled');
+    const { album } = store.getState().albums;
+    expect(Object.keys(album).length).toBeGreaterThan(0);
+  });
 
-  //   // expect(state.users).toContain(getUserListResponse[0]);
-  // });
+  it('should be able to fetch the album albums', async () => {
+    const result = await store.dispatch(fetchAlbumPhotos(albumId));
 
+    expect(result.type).toBe('albums/fetchAlbumPhotos/fulfilled');
+    const { albumPhotos } = store.getState().albums;
+    expect(albumPhotos.length).toBeGreaterThan(0);
+  });
 });
-
